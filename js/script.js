affcc1cedddd5c0fa3dd44758c6ae0faa9e62ddd
JazminMonzon <jazminmonzonarancibia@gmail.com>
2021-02-19 00:19:41 -0300

    Agregando Checkout

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
 let carrito = [];
 
 class Pelicula{
-    constructor(nombrePelicula, precioAlquiler, imagenPelicula){
+    constructor(nombrePelicula, precioAlquiler, imagenPelicula, linkPelicula, idPelicula){
         this.nombre = nombrePelicula;
         this.precio = precioAlquiler;
         this.imagen = imagenPelicula;
+        this.link = linkPelicula;
+        this.id = idPelicula;
     }
 }
 
 let baseDeDatos = []
 
-let peliculaUno = new Pelicula("La muerte no existe y el amor tampoco", 200, "../imagenes/Afiche LMNE.jpg");
-let peliculaDos = new Pelicula("Araña", 200, "../imagenes/Afiche Arana.jpg");
-let peliculaTres = new Pelicula("El cuidado de los otros", 100, "../imagenes/Afiche ECDLO.jpg");
-let peliculaCuatro = new Pelicula("Los adoptantes", 100, "../imagenes/Afiche Los adoptantes.png");
-let peliculaCinco = new Pelicula("Las buenas intenciones", 100, "../imagenes/Afiche LBI.jpg");
-let peliculaSeis = new Pelicula("El motoarrebatador", 100, "../imagenes/Afiche El motoarrebatador.jpg");
+let peliculaUno = new Pelicula("La muerte no existe y el amor tampoco", 200, "../imagenes/Afiche LMNE.jpg", "#", 1);
+let peliculaDos = new Pelicula("Araña", 200, "../imagenes/Afiche Arana.jpg", "#", 2);
+let peliculaTres = new Pelicula("El cuidado de los otros", 100, "../imagenes/Afiche ECDLO.jpg", "#", 3);
+let peliculaCuatro = new Pelicula("Los adoptantes", 100, "../imagenes/Afiche Los adoptantes.png", "#", 4);
+let peliculaCinco = new Pelicula("Las buenas intenciones", 100, "../imagenes/Afiche LBI.jpg", "../views/lasbuenasintenciones.html", 5);
+let peliculaSeis = new Pelicula("El motoarrebatador", 100, "../imagenes/Afiche El motoarrebatador.jpg", "../views/elmotoarrebatador.html", 6);
 
 
 baseDeDatos.push(peliculaUno);
@@ -30,14 +32,14 @@ for (let i = 0; i < baseDeDatos.length; i++) {
     aux += `
         <div class="col-sm-4 mb-4 mt-4">
             <div class="card h-100 text-center">
-                <a href="#"><img class="card-img-top" src="${baseDeDatos[i].imagen}" alt=""></a>
+                <a href="${baseDeDatos[i].link}"><img class="card-img-top" src="${baseDeDatos[i].imagen}" alt=""></a>
                 <div class="card-body">
+                    <h5>${baseDeDatos[i].id}</h5>
                     <h3 class="card-title">
-                        <a href="#">${baseDeDatos[i].nombre}</a>
+                        <a href="${baseDeDatos[i].link}" style="text-decoration: none">${baseDeDatos[i].nombre}</a>
                     </h3>
                     <h4>$${baseDeDatos[i].precio}</h4>
                 </div>
-                <br>
                 <div class="card-footer">
                     <button class="btn btn-outline-success my-2 my-sm-0" style="color: black;" onclick='agregarAlCarrito(${JSON.stringify(baseDeDatos[i])})'>Alquilar</button>
                 </div>
@@ -70,7 +72,7 @@ function agregarAlCarrito(peli){
     //                                                    ${aux2} 
     //                                                    Precio total: $ ${aux}
     //                                                    `;
-    $("#precio-total").fadeOut()
+    $("#precio-total").fadeOut(400)
     $("#precio-total").fadeIn()
     $("#precio-total").css("color", "green")
     $("#precio-total").css("fontSize", "18px")
@@ -105,3 +107,34 @@ function borrarUnProducto(){
 localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
 carrito = nuevoCarrito;
 }
+
+function vaciarCarrito(){
+    const  nuevoCarrito = [];
+    document.getElementById("precio-total").innerHTML = `No hay películas seleccionadas`; 
+    console.log(nuevoCarrito);
+    localStorage.setItem("carrito", JSON.stringify(nuevoCarrito));
+    carrito = nuevoCarrito;
+}
+
+// CHECKOUT
+let auxCheckout = ``;
+let auxPrecioTotal = 0;
+for (let i = 0; i < carrito.length; i++) {
+    auxPrecioTotal += carrito[i].precio;
+    auxCheckout += `
+    <div class="container-carrito">
+    <h4>Carrito
+      <span class="price" style="color:black">
+        <i class="fa fa-shopping-cart"></i>
+        <b>${carrito.length}</b>
+      </span>
+    </h4>
+    <p><a href="${carrito[i].link}" style="text-decoration: none">${carrito[i].nombre}</a> <span class="price">$${carrito[i].precio}</span></p>
+    <hr>
+    <p>Total <span class="price" style="color:black"><b>$${auxPrecioTotal}</b></span></p>
+  </div>
+</div>
+        `;
+  }
+
+document.getElementById("checkout").innerHTML = auxCheckout;
